dfce75c91b244be8716aa724967099c16514965e
apis <hafizadha166@gmail.com>
2023-11-28 15:20:59 +0700

    test: update unit test springboot oauth-sso

--- src/test/java/demo/app/controller/AddressControllerTest.java
+++ src/test/java/demo/app/controller/AddressControllerTest.java
@@ -5,21 +5,18 @@ import demo.app.model.AddressRequest;
 import demo.app.model.AddressResponse;
 import demo.app.model.WebResponse;
 import demo.app.service.AddressService;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AddressController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -32,33 +29,39 @@ public class AddressControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    @Disabled
     void testUpdateValidAddressRequestShouldReturnAddressResponse() throws Exception {
-        AddressRequest addressRequest = new AddressRequest();
-        addressRequest.setStreet("123 Main St");
-        addressRequest.setCity("City");
-        addressRequest.setProvince("Province");
-        addressRequest.setCountry("Country");
-        addressRequest.setPostalCode("12345");
+        AddressRequest request = new AddressRequest();
+        request.setStreet("Test");
+        request.setCity("Test");
+        request.setProvince("Test");
+        request.setCountry("Test");
+        request.setPostalCode("Test");
 
-        AddressResponse addressResponse = new AddressResponse();
-        addressResponse.setAddressId("1");
-        addressResponse.setStreet("123 Main St");
-        addressResponse.setCity("City");
-        addressResponse.setProvince("Province");
-        addressResponse.setCountry("Country");
-        addressResponse.setPostalCode("12345");
-        given(addressService.updateAddress(any(AddressRequest.class), any(OAuth2AuthenticatedPrincipal.class))).willReturn(addressResponse);
+        AddressResponse response = new AddressResponse();
+        response.setAddressId("1");
+        response.setStreet("Test");
+        response.setCity("Test");
+        response.setProvince("Test");
+        response.setCountry("Test");
+        response.setPostalCode("Test");
+
+        given(addressService.updateAddress(any(AddressRequest.class), any())).willReturn(response);
 
         mockMvc.perform(put("/api/address/current")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(addressRequest)))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(new WebResponse<>(addressResponse, null))))
-                .andReturn();
+                .andExpect(content().json(objectMapper.writeValueAsString(new WebResponse<>(response, null))))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.addressId").value("1"))
+                .andExpect(jsonPath("$.data.street").value("Test"))
+                .andExpect(jsonPath("$.data.city").value("Test"))
+                .andExpect(jsonPath("$.data.province").value("Test"))
+                .andExpect(jsonPath("$.data.country").value("Test"))
+                .andExpect(jsonPath("$.data.postalCode").value("Test"));
 
-        verify(addressService, times(1)).updateAddress(addressRequest, any(OAuth2AuthenticatedPrincipal.class));
+        verify(addressService, times(1)).updateAddress(any(AddressRequest.class), any());
     }
 }
--- src/test/java/demo/app/controller/ReimbursementControllerTest.java
+++ src/test/java/demo/app/controller/ReimbursementControllerTest.java
@@ -1,23 +1,20 @@
 package demo.app.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.app.entity.Reimbursement;
 import demo.app.model.ReimbursementRequest;
 import demo.app.model.ReimbursementResponse;
 import demo.app.model.WebResponse;
 import demo.app.service.ReimbursementService;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
-import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -34,13 +31,10 @@ public class ReimbursementControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private ReimbursementService reimbursementService;
-    @Mock
-    private OAuth2AuthenticatedPrincipal principal;
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    @Disabled
     void testCreateReimbursementWhenValidRequestThenReturnCreated() throws Exception {
         ReimbursementRequest request = new ReimbursementRequest();
         request.setAmount(new BigDecimal(1000));
@@ -53,8 +47,7 @@ public class ReimbursementControllerTest {
         response.setActivity("Travel");
         response.setTypeReimbursement("Transport");
         response.setDescription("Travel to client");
-
-        given(reimbursementService.create(any(ReimbursementRequest.class), any(OAuth2AuthenticatedPrincipal.class))).willReturn(response);
+        given(reimbursementService.create(any(ReimbursementRequest.class), any())).willReturn(response);
 
         mockMvc.perform(post("/api/reimbursements")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -62,17 +55,24 @@ public class ReimbursementControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(objectMapper.writeValueAsString(new WebResponse<>(response, null))))
-                .andReturn();
+                .andExpect(jsonPath("data.reimbursementId").value(1L))
+                .andExpect(jsonPath("data.amount").value("Rp1.000,00"))
+                .andExpect(jsonPath("data.activity").value("Travel"))
+                .andExpect(jsonPath("data.typeReimbursement").value("Transport"))
+                .andExpect(jsonPath("data.description").value("Travel to client"));
 
-        verify(reimbursementService, times(1)).create(any(ReimbursementRequest.class), any(OAuth2AuthenticatedPrincipal.class));
-
+        verify(reimbursementService, times(1)).create(any(ReimbursementRequest.class), any());
     }
 
-
     @Test
-    @Disabled
     void testUpdateReimbursementWhenValidRequestThenReturnOk() throws Exception {
-        long reimbursementId = 1L;
+        Reimbursement reimbursement = new Reimbursement();
+        reimbursement.setReimbursementId(1L);
+        reimbursement.setAmount(BigDecimal.valueOf(1000.00));
+        reimbursement.setDescription("Expense reimbursement");
+        reimbursement.setActivity("Travel");
+        reimbursement.setTypeReimbursement("Transport");
+
         ReimbursementRequest request = new ReimbursementRequest();
         request.setAmount(new BigDecimal(2000));
         request.setActivity("Meal");
@@ -85,28 +85,29 @@ public class ReimbursementControllerTest {
         response.setTypeReimbursement("Food");
         response.setDescription("Lunch with client");
 
-        given(reimbursementService.updateReimbursementUser(anyLong(), any(ReimbursementRequest.class), any(OAuth2AuthenticatedPrincipal.class))).willReturn(response);
+        given(reimbursementService.updateReimbursementUser(anyLong(), any(ReimbursementRequest.class), any())).willReturn(response);
 
-        mockMvc.perform(patch("/api/reimbursements/" + reimbursementId)
+        mockMvc.perform(patch("/api/reimbursements/" + 1L)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(new WebResponse<>(response, null))))
                 .andExpect(jsonPath("$.data.amount").value("Rp2.000,00"))
                 .andExpect(jsonPath("$.data.activity").value("Meal"))
                 .andExpect(jsonPath("$.data.typeReimbursement").value("Food"))
                 .andExpect(jsonPath("$.data.description").value("Lunch with client"));
 
-        verify(reimbursementService, times(1)).updateReimbursementUser(anyLong(), any(ReimbursementRequest.class), any(OAuth2AuthenticatedPrincipal.class));
+        verify(reimbursementService, times(1)).updateReimbursementUser(anyLong(), any(ReimbursementRequest.class), any());
     }
 
     @Test
     void testDeleteReimbursementWhenValidRequestThenReturnOk() throws Exception {
-        given(principal.getAttributes()).willReturn(Map.of("sub", "123"));
-
-        doNothing().when(reimbursementService).removeReimbursementByUser(anyLong(), eq(principal));
+        doNothing().when(reimbursementService).removeReimbursementByUser(anyLong(), any());
 
         mockMvc.perform(delete("/api/reimbursements/" + 1L))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.data").value("Reimbursement deleted"));
     }
 }
